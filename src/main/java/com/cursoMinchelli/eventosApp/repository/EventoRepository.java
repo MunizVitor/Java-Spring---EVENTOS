982630c8042a2ded66ccee3ab319ccb298b7084a
MunizVitor <vmuniz.dev@gmail.com>
2026-03-07 23:59:25 -0300

    imp... get no html

--- src/main/java/com/cursoMinchelli/eventosApp/repository/EventoRepository.java
+++ src/main/java/com/cursoMinchelli/eventosApp/repository/EventoRepository.java
@@ -1,4 +1,7 @@
 package com.cursoMinchelli.eventosApp.repository;
 
-public interface EventoRepository {
+import com.cursoMinchelli.eventosApp.model.Evento;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventoRepository extends JpaRepository<Evento, Long> {
 }
