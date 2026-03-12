6697ac2f9359e1e1fe999bb34a67faf5856a9f20
MunizVitor <vmuniz.dev@gmail.com>
2026-03-12 15:04:33 -0300

    Criando tela de detalhes de evento

--- src/main/java/com/cursoMinchelli/eventosApp/controller/EventoController.java
+++ src/main/java/com/cursoMinchelli/eventosApp/controller/EventoController.java
@@ -2,11 +2,15 @@ package com.cursoMinchelli.eventosApp.controller;
 
 import com.cursoMinchelli.eventosApp.model.Evento;
 import com.cursoMinchelli.eventosApp.repository.EventoRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class EventoController {
@@ -30,6 +34,14 @@ public class EventoController {
         ModelAndView mv = new ModelAndView("/index");
         Iterable<Evento> eventos = repository.findAll();
         mv.addObject("eventos", eventos);
+        return mv;
+    }
+
+    @RequestMapping("/{id}")
+    public ModelAndView detalhesEvento(@PathVariable("id") long id){
+        Evento evento = repository.findById(id).orElseThrow(() -> new RuntimeException("Evento não encontrado"));
+        ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+        mv.addObject("evento", evento);
         return mv;
     }
 }
